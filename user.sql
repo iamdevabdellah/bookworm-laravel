3f51113828fd211832986d12919b54c40bff9809
Abdellah Ahmed <24692961+iamdevabdellah@users.noreply.github.com>
2019-07-05 09:50:15 +0600

    Create user.sql

---
+++ user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@admin.com', NULL, '$2y$10$97C0J2HEla/KmoTbyqbQVul9oyHX8UTdjD0EwjhfwRJxCEcnMuMyG', NULL, '2019-06-27 21:55:36', '2019-06-27 21:55:36');
